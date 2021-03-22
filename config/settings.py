6019c0b9d091b75cb0f65ef93d0616816cab04ce
Seiya Asahina <seiya@asahina>
2021-03-22 11:00:32 +0900

    fixed

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY  = "asdasda"
+SECRET_KEY  = "GENERATED KEY "
 try:
     from .local_settings import *
 except ImportError:
